19e1555bd7fed7add78a57091f86452e3cb71005
Daniyar <damai3350@gmail.com>
2025-08-14 18:19:23 +0300

    Реализован проверка файла на наличие, его создание в противном случае. Чтение построчное из файла и запись прсточная в файл

---
+++ internal/storage/storage.go
@@ -0,0 +1,125 @@
+package storage
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Expense_csv struct {
+	ID          int
+	Description string
+	Amount      float64
+	Date        time.Time
+	Category    string
+}
+
+func CreateData() error {
+	file, err := os.Create("data/expenses.csv")
+	if err != nil {
+		return fmt.Errorf("Ошибка создания файла: %w", err)
+	}
+	writer := csv.NewWriter(file)
+	err = writer.Write([]string{"ID", "Description", "Amount", "Date", "Category"})
+	if err != nil {
+		return fmt.Errorf("Ошибка записи заголовков: %w", err)
+	}
+	writer.Flush()
+
+	defer file.Close()
+	return nil
+}
+func CheckData() error {
+	_, err := os.Stat("data/expenses.csv")
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("Файл не существует")
+			fmt.Println("Создаем файл...")
+			err = CreateData()
+			if err != nil {
+				return fmt.Errorf("Ошибка создания файла: %w", err)
+			}
+			return nil
+		}
+		return fmt.Errorf("Ошибка открытия файла: %w", err)
+	}
+
+	return nil
+}
+func LoadData() ([]Expense_csv, error) {
+	file, err := os.Open("data/expenses.csv")
+	if err != nil {
+		return nil, fmt.Errorf("Ошибка открытия файла: %w", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	if _, err := reader.Read(); err != nil {
+		return nil, fmt.Errorf("Ошибка чтения заголовков: %w", err)
+	}
+
+	var tracks []Expense_csv
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			fmt.Println("Файл успешно загружен")
+			break
+		} else if err != nil {
+			return nil, fmt.Errorf("Ошибка загрузки файла: %w", err)
+		}
+		id, err := strconv.Atoi(record[0])
+		if err != nil {
+			return nil, fmt.Errorf("Ошибка преобразования ID: %w", err)
+		}
+
+		amount, err := strconv.ParseFloat(record[2], 64)
+		if err != nil {
+			return nil, fmt.Errorf("Ошибка преобразования внесенной суммы: %w", err)
+
+		}
+		date, err := time.Parse("2006-01-02", record[3])
+		if err != nil {
+			return nil, fmt.Errorf("Ошибка преобразования даты: %w", err)
+		}
+		if len(record) < 5 {
+			return nil, fmt.Errorf("Недостаточно данных в строке")
+		}
+
+		tracks = append(tracks, Expense_csv{
+			ID:          id,
+			Description: record[1],
+			Amount:      amount,
+			Date:        date,
+			Category:    record[4],
+		})
+	}
+	return tracks, nil
+}
+
+func SaveData(expense []Expense_csv) error {
+	file, err := os.OpenFile("data/expenses.csv", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("Ошибка создания файла: %w", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	for _, e := range expense {
+		row := []string{
+			strconv.Itoa(e.ID),
+			e.Description,
+			strconv.FormatFloat(e.Amount, 'f', 2, 64),
+			e.Date.Format("2006-01-02"),
+			e.Category,
+		}
+		if err := writer.Write(row); err != nil {
+			return fmt.Errorf("Ошибка записи в файл: %w", err)
+		}
+	}
+
+	return nil
+}
